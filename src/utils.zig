2b91303860d6cb47e0f2e18026b2b835eb5181cc
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2025-02-01 18:16:25 +0200

    update usage

--- src/utils.zig
+++ src/utils.zig
@@ -1,5 +1,6 @@
 pub const Utils = struct {
-    pub const USAGE_GENERAL = "cliorc path/to/save_file.json l|reg";
+    pub const USAGE_GENERAL = "cliorc path/to/save_file.json l|reg|rcv";
     pub const USAGE_LOGIN = "cliorc path/to/save_file.json l instance-url.com username password";
     pub const USAGE_REGISTER = "cliorc path/to/save_file.json reg instance-url.com username password";
+    pub const USAGE_RECEIVE = "cliorc path/to/save_file.json rcv"; // not really used
 };
